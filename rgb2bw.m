a414f924f9b08f536218b4f51e8922e4d4043e6a
matteomts <hmateus1997@gmail.com>
2016-04-08 08:12:02 -0300

    Update rgb2bw.m

--- rgb2bw.m
+++ rgb2bw.m
@@ -1,1 +1,15 @@
-// converter rgb para grayscale
+%converter rgb para grayscale
+
+I = imgread('imagem.jpg'); %vai ler a imagemI
+
+figure, imshow (I) %apenas exibe a imagem I
+
+I_Cinza = rgb2gray(I); %converte a imagem I para tons de cinza
+
+figure, imshow(I_cinza)
+
+b = im2bw(I_cinza); %o im2bw fará a compressão da imagem em tons de cinza para uma imagem binária
+
+figure, imshow(b) %mostra a imagem convertida para binário
+
+end
