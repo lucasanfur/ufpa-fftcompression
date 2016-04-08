43a8922aa3f9afb0630985b8c3cfa966f796ad6f
lucasanfur <lucasanfur@gmail.com>
2016-04-08 01:30:43 -0300

    Update rgb2bw.m
    codigo rgb2bw
    
    lucas santiago

--- rgb2bw.m
+++ rgb2bw.m
@@ -1,1 +1,15 @@
 // converter rgb para grayscale
+
+
+I = imread ('imagem.jpg' ); // será feita a leitura da imagem I
+
+figure, imshow(I) 
+
+I_Cinza = rgb2gray(I); vai transformar a imagem I para cinza
+
+figure, imshow(I_cinza) 
+
+b = im2bw(I_Cinza); a imagem I que esta em cinza será convertida para uma imagem binaria 
+
+
+figure, imshow(b)
